41cfe1971186aac9cd88b64ef3022e97ea16f0bd
peigne <peigne@peigne.local>
2020-10-19 19:12:04 +0200

    add lib

---
+++ tools/Bolt/FragmentListener/lib/phpggc/lib/PHPGGC/Enhancement/PlusNumbers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHPGGC\Enhancement;
+
+/**
+ * Adds a + (plus) symbol before every integer symbol of given type.
+ * For instance, with 'Osi',
+ * O:3:"Abc":1:{s:1:"x";i:3;} -> O:+3:"Abc":+1:{s:+1:"x";i:+3;}
+ * With 's':
+ * O:3:"Abc":1:{s:1:"x";i:3;} -> O:3:"Abc":1:{s:+1:"x";i:3;}
+ *
+ * Note: Since PHP 7.2, only i and d (float) types can have a +.
+ */
+class PlusNumbers extends Enhancement
+{
+    private $types;
+
+    public function __construct($types)
+    {
+        $this->types = $types;
+    }
+
+    public function process_serialized($serialized)
+    {
+        $types = preg_quote($this->types, '#');
+        $serialized = preg_replace(
+            '#\b([' . $types . ']):(\d+)([:;])#',
+            '$1:+$2$3',
+            $serialized
+        );
+        return $serialized;
+    }
+}
